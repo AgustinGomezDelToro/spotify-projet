feed5e8ff3e3cf514d10376934712c1714a5911c
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2024-04-23 15:14:56 +0200

    back finished

---
+++ back/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+import { userRouter } from './routes/user.router.ts';
+
+export const prisma = new PrismaClient();
+
+export const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/user', userRouter);
+
+app.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
+});
